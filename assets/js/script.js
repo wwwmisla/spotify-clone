72afe3ec0a3083d5758e4a952a8ef67659980ea8
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-26 18:50:02 -0300

    :sparkles: feat: Ajustes na Api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById("result-artist");
+const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
     fetch(url)
         .then((response) => response.json())
         .then((result) => displayResults(result))
@@ -27,8 +27,8 @@ document.addEventListener('input', function () {
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
         resultArtist.classList.remove('hidden');
-        return;
+        return
     }
-
-    requestApi(searchTerm);
+    
+    console.log(requestApi(searchTerm));
 })
