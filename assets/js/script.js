ea78fe57a1124d8798d2717ccc53637522e28866
Misla Wislaine <mislawislaine12@gmail.com>
2024-01-27 15:46:05 -0300

    :sparkles: feat: Reajuste nos scripts
    - Divisão de dois arquivos js;
    - Criação de um script para dar bom dia, boa tarde ou boa noite;
    - Criação de um script para colocar o ano atual no copy.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,18 @@
-const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist');
-const resultPlaylist = document.getElementById('result-playlists');
+// Dizer Bom dia, boa tarde ou boa noite, conforme a hora atual.
+const hora = new Date().getHours();
+const greetingElement = document.getElementById("greeting");
 
-function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result))
+let horarioAtual;
+
+if (hora >= 0 && hora < 12) {
+    horarioAtual = "Bom dia!";
+} else if (hora >= 12 && hora < 18) {
+    horarioAtual = "Boa tarde!";
+} else {
+    horarioAtual = "Boa noite!";
 }
 
-function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+greetingElement.textContent = horarioAtual;
 
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
-    });
-
-    resultArtist.classList.remove('hidden');
-}
-
-document.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
-    }
-    
-    console.log(requestApi(searchTerm));
-})
+// Inserir o ano atual no copy
+copy.innerHTML += new Date().getFullYear();
